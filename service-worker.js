e3d95955697d7a87d1a28eedea616720471bb9c4
KOH-app <kiyoceanfield@gmail.com>
2026-04-16 15:38:37 +0900

    service-worker.jsを追加

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'meal-record-v1';
+const urlsToCache = [
+    '/meal-record/',
+    '/meal-record/index.html',
+    '/meal-record/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
