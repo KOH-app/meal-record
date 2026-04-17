58d6a716a4cb01ecb5e3f92cf4e89e3148cdc3c7
KOH-app <kiyoceanfield@gmail.com>
2026-04-17 10:58:55 +0900

    古いキャッシュを自動削除

--- service-worker.js
+++ service-worker.js
@@ -12,6 +12,17 @@ self.addEventListener('install', (event) => {
     );
 });
 
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        })
+    );
+});
+
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
